ff822bdf053e622e83e9168298d16c3950e786f8
paramono <alex@paramono.com>
2016-02-10 19:31:12 +0300

    Add git templates

--- autoserver.py
+++ autoserver.py
@@ -8,8 +8,29 @@ from string import Template
 from shutil import copyfile
 from subprocess import call
 
+
+def instance_template(service_name, temp_path, dest_path, overwrite=False, **kwargs):
+    if is_writable(dest_path, overwrite):
+        # open original template
+        with open(temp_path, 'r') as temp_file:
+            temp   = Template(temp_file.read())
+            output = temp.substitute(**kwargs)
+
+        # create a template instance
+        with open(dest_path, 'w') as dest_file:
+            # print('%s: writing [%s]' % (service_name, dest_path))
+            dest_file.write(output)
+
+
+def copy_template(service_name, temp_path, dest_path, overwrite=False, **kwargs):
+    if is_writable(dest_path, overwrite):
+        # print('%s: writing [%s]' % (service_name, dest_path))
+        copyfile(temp_path, dest_path)
+
+
 def print_hr(length=12, end='\n'):
     print('* ' * length + end)
+
 
 def is_valid_ip(arg):
     try:
@@ -28,13 +49,13 @@ def is_valid_path(arg):
 
 def is_writable(path, overwrite):
     if not os.path.exists(path):
+        print('Creating [%s]' % path)
         return True
+    if overwrite:
+        print('path [%s] exists, overwriting' % path)
     else:
-        if overwrite:
-            print('path [%s] exists, overwriting' % path)
-        else:
-            print('path [%s] exists, skipping' % path)
-        return overwrite
+        print('path [%s] exists, skipping' % path)
+    return overwrite
 
 
 def is_valid_hostname(arg):
@@ -51,7 +72,6 @@ def is_valid_hostname(arg):
 
 
 if __name__ == '__main__':
-
     try:
         script_path = os.readlink(__file__)
     except OSError:
@@ -94,78 +114,73 @@ if __name__ == '__main__':
     # boolean switches
     parser.add_argument('--skip-nginx', action='store_true')
     parser.add_argument('--skip-uwsgi', action='store_true')
-    parser.add_argument('--skip-git',   action='store_true')
     parser.add_argument('--skip-venv',  action='store_true')
+    parser.add_argument('--skip-git',   action='store_true')
+    parser.add_argument('--deploy',     action='store_true')
     parser.add_argument('-w', '--overwrite', action='store_true')
 
     args = vars(parser.parse_args())
 
-    glob_vars = [
-        'target_dir',
-        'skip_nginx', 
-        'skip_uwsgi',
-        'skip_git',
-        'skip_venv',
-        'overwrite',
-        'ip',
-        'domain',
-        'proj',
-    ]
+    # glob_vars = [
+    #     'target_dir',
+    #     'skip_nginx', 
+    #     'skip_uwsgi',
+    #     'skip_venv',
+    #     'skip_git',
+    #     'deploy',
+    #     'overwrite',
+    #     'ip',
+    #     'domain',
+    #     'proj',
+    # ]
 
     locals().update(
-        {k: v for (k, v) in args.items() if k in glob_vars}
+        # {k: v for (k, v) in args.items() if k in glob_vars}
+        {k: v for (k, v) in args.items()}
     )
 
     # template locations
-    te_nginx_filename  = os.path.join(template_dir, 'nginx_proj.conf')
-    te_nginx_https     = os.path.join(template_dir, 'nginx_https.conf')
-    te_nginx_redirects = os.path.join(template_dir, 'nginx_redirects.conf')
+    te_nginx_conf      = os.path.join(template_dir, 'nginx_proj.conf')
+    te_nginx_https     = os.path.join(template_dir, 'nginx_proj_https.conf')
+    te_nginx_redirects = os.path.join(template_dir, 'nginx_proj_redirects.conf')
     te_uwsgi_emperor   = os.path.join(template_dir, 'uwsgi_emperor_proj.service')
     te_uwsgi_proj      = os.path.join(template_dir, 'uwsgi_proj.ini')
+    te_git_hook        = os.path.join(template_dir, 'post-receive')
 
     has_domain_and_ip = bool(args['domain'] and args['ip'])
 
+    proj_dir = os.path.join(target_dir, proj)
+    os.makedirs(proj_dir, exist_ok=overwrite)
+
     if not all((skip_nginx, skip_uwsgi)):
-        target_conf_dir = os.path.join(target_dir, proj, 'conf')
-        os.makedirs(target_conf_dir, exist_ok=True)
+        conf_dir = os.path.join(proj_dir, 'conf')
+        os.makedirs(conf_dir, exist_ok=True)
+
 
     if skip_nginx:
-        print("* * * Skipping nginx")
+        print("\n* * * Skipping nginx")
     elif ip and domain: # only if ip and domain present
         print("* * * Creating nginx configs")
         # nginx target directories
-        nginx_filename  = os.path.abspath(
-            os.path.join(target_conf_dir, '%s_conf.nginx' % args['proj']))
+        nginx_conf      = os.path.abspath(
+            os.path.join(conf_dir, 'nginx_%s.conf' % proj))
         nginx_https     = os.path.abspath(
-            os.path.join(target_conf_dir, '%s_https.nginx' % args['proj']))
+            os.path.join(conf_dir, 'nginx_%s_https.conf' % proj))
         nginx_redirects = os.path.abspath(
-            os.path.join(target_conf_dir, '%s_redirects.nginx' % args['proj']))
+            os.path.join(conf_dir, 'nginx_%s_redirects.conf' % args['proj']))
 
-        nginx_vars = ['nginx_filename', 'nginx_https', 'nginx_redirects']
+        nginx_vars = ['nginx_conf', 'nginx_https', 'nginx_redirects']
         local_vars = locals()
         args.update({k:local_vars[k] for k in nginx_vars})
 
-        if is_writable(nginx_filename, overwrite):
-            # open original template
-            with open(te_nginx_filename, 'r') as te_nginx_file:
-                nginx_template = Template(te_nginx_file.read())
-                nginx_str = nginx_template.substitute(**args)
-
-            # write a config from a "rendered" template
-            with open(nginx_filename, 'w') as nginx_file:
-                print('nginx: writing [%s]' % nginx_filename)
-                nginx_file.write(nginx_str)
+        # instance the main nginx project config
+        instance_template('nginx', te_nginx_conf, nginx_conf, **args)
 
         # https forcing snippet (commented out)
-        if is_writable(nginx_https, overwrite):
-            print('nginx: writing [%s]' % nginx_https)
-            copyfile(te_nginx_https, nginx_https)
+        copy_template('nginx', te_nginx_https, nginx_https, **args)
 
         # redirect list snippet (commented out)
-        if is_writable(nginx_redirects, overwrite):
-            print('nginx: writing [%s]' % nginx_redirects)
-            copyfile(te_nginx_redirects, nginx_redirects)
-
+        copy_template('nginx', te_nginx_redirects, nginx_redirects, **args)
         print_hr()
 
 
@@ -179,9 +194,26 @@ if __name__ == '__main__':
         print('* * * Skipping virtualenv')
     else:
         # create dir
-        target_env_dir = os.path.join(target_dir, proj, 'env{proj}'.format(proj=proj))
+        target_env_dir = os.path.join(proj_dir, 'env%s' % proj)
 
-        print('\n* * * Running virtualenv')
+        print('* * * Running virtualenv')
         call(['virtualenv', '-p', 'python3', target_env_dir])
         print_hr()
+
+
+    # create git repo only at deployment server
+    if skip_git or not deploy:
+        print('* * * Skipping git repo')
+    else:
+        print('* * * Creating git bare repo')
+        target_repo_dir = os.path.join(target_dir, proj, 'repo.git')
+        call(['git', 'init', '--bare', target_repo_dir])
+
+        repo_hooks = os.path.join(target_repo_dir, 'hooks')
+
+        git_hook = os.path.abspath(
+            os.path.join(repo_hooks, 'post-receive'))
+
+        instance_template('git', te_git_hook, git_hook, **args)
+
 
