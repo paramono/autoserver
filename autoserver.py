825f04d70b0cd6a942fa8d46169099363da446d4
paramono <alex@paramono.com>
2016-02-10 21:07:11 +0300

    Update templates

--- autoserver.py
+++ autoserver.py
@@ -12,13 +12,12 @@ from util.validators import (
         is_valid_ip, 
         is_valid_hostname, 
         is_valid_path
-)
+        )
 from util.template_helpers import instance_template, copy_template
+
 
 def print_hr(length=40, end='\n'):
     print('-' * length + end)
-
-
 
 
 if __name__ == '__main__':
@@ -44,12 +43,12 @@ if __name__ == '__main__':
         )
     parser.add_argument('-p', '--proj', 
         type=str, 
-        help='project name',
+        help='global project name',
         required=True
         )
-    parser.add_argument('-a', '--app', 
+    parser.add_argument('-j', '--django-proj', 
         type=str, 
-        help='application name',
+        help='django project name',
         required=True
         )
     parser.add_argument('-i', '--ip', 
@@ -72,25 +71,12 @@ if __name__ == '__main__':
     parser.add_argument('--skip-venv',  action='store_true')
     parser.add_argument('--skip-git',   action='store_true')
     parser.add_argument('--production', action='store_true')
+    parser.add_argument('--deploy',     action='store_true')
     parser.add_argument('-w', '--overwrite', action='store_true')
+    parser.add_argument('--force-reinit-git', action='store_true')
 
     args = vars(parser.parse_args())
-
-    # glob_vars = [
-    #     'target_dir',
-    #     'skip_nginx', 
-    #     'skip_uwsgi',
-    #     'skip_venv',
-    #     'skip_git',
-    #     'deploy',
-    #     'overwrite',
-    #     'ip',
-    #     'domain',
-    #     'proj',
-    # ]
-
     locals().update(
-        # {k: v for (k, v) in args.items() if k in glob_vars}
         {k: v for (k, v) in args.items()}
     )
 
@@ -99,18 +85,13 @@ if __name__ == '__main__':
     te_nginx_https     = os.path.join(template_dir, 'nginx_proj_https.conf')
     te_nginx_redirects = os.path.join(template_dir, 'nginx_proj_redirects.conf')
     te_uwsgi_emperor   = os.path.join(template_dir, 'uwsgi_emperor_proj.service')
-    te_uwsgi_proj      = os.path.join(template_dir, 'uwsgi_proj.ini')
+    te_uwsgi_ini       = os.path.join(template_dir, 'uwsgi_proj.ini')
     te_git_hook        = os.path.join(template_dir, 'post-receive')
 
     has_domain_and_ip = bool(args['domain'] and args['ip'])
 
+    # create project dir (working dir for repo)
     proj_dir = os.path.join(target_dir, proj)
-    os.makedirs(proj_dir, exist_ok=overwrite)
-
-    if not all((skip_nginx, skip_uwsgi)):
-        conf_dir = os.path.join(proj_dir, 'conf')
-        os.makedirs(conf_dir, exist_ok=True)
-
 
     if skip_venv:
         print('* * * Skipping virtualenv')
@@ -126,6 +107,20 @@ if __name__ == '__main__':
     if not production:
         print("* * * Production mode off, deploying locally")
     else:
+        # create conf directory
+        if not all((skip_nginx, skip_uwsgi)):
+            conf_dir = os.path.join(proj_dir, 'conf')
+            os.makedirs(conf_dir, exist_ok=True)
+
+        django_proj_dir = os.path.join(proj_dir, django_proj)
+        static_dir      = os.path.join(proj_dir, 'global_static')
+        backups_dir     = os.path.join(proj_dir, 'backups')
+
+        os.makedirs(django_proj_dir, exist_ok=True)
+        os.makedirs(static_dir,      exist_ok=True)
+        os.makedirs(backups_dir,     exist_ok=True)
+
+        # handle nginx
         if skip_nginx:
             print("\n* * * Skipping nginx")
         elif ip and domain: # only if ip and domain present
@@ -138,6 +133,8 @@ if __name__ == '__main__':
             nginx_redirects = os.path.abspath(
                 os.path.join(conf_dir, 'nginx_%s_redirects.conf' % args['proj']))
 
+            # make some locals available for templates
+            # (including absolute paths)
             nginx_vars = ['nginx_conf', 'nginx_https', 'nginx_redirects']
             local_vars = locals()
             args.update({k:local_vars[k] for k in nginx_vars})
@@ -153,31 +150,50 @@ if __name__ == '__main__':
             print_hr()
 
 
+        # handle uwsgi
         if skip_uwsgi:
             print('* * * Skipping uwsgi')
         else:
-            pass
+            print("* * * Creating uwsgi configs")
+            uwsgi_emperor = os.path.abspath(
+                os.path.join(conf_dir, 'uwsgi_emperor_%s.service' % proj))
+            uwsgi_ini = os.path.abspath(
+                os.path.join(conf_dir, 'uwsgi_%s.ini' % proj))
+
+            instance_template('uwsgi', te_uwsgi_emperor, uwsgi_emperor, **args)
+            instance_template('uwsgi', te_uwsgi_ini,     uwsgi_ini,     **args)
+            print_hr()
+
 
         # create git repo
         if skip_git:
             print('* * * Skipping git repo')
         else:
-            print('* * * Creating git bare repo')
-            repo_dir = os.path.join(target_dir, proj, 'repo.git')
-            call(['git', 'init', '--bare', repo_dir])
+            repo_dir = os.path.join(proj_dir, 'repo.git')
+            if not os.path.exists(repo_dir) or force_reinit_git:
+                print('* * * Creating git bare repo')
+                call(['git', 'init', '--bare', repo_dir])
+                repo_hooks = os.path.join(repo_dir, 'hooks')
 
-            repo_hooks = os.path.join(repo_dir, 'hooks')
+                git_hook = os.path.abspath(
+                    os.path.join(repo_hooks, 'post-receive'))
 
-            git_hook = os.path.abspath(
-                os.path.join(repo_hooks, 'post-receive'))
+                instance_template('git', te_git_hook, git_hook, **args)
+                os.chmod(git_hook, 0o755)
 
-            instance_template('git', te_git_hook, git_hook, **args)
-
-            print('\n>>> Run this on your workstation <<<')
-            print(
-                'git remote add live ssh://{username}@{domain}{repo_dir}'
-                .format(username=user, domain=domain, repo_dir=repo_dir)
-            )
+                print('\n>>> Run this on your workstation <<<')
+                print(
+                    'git remote add live ssh://{username}@{domain}{repo_dir}'
+                    .format(username=user, domain=domain, repo_dir=repo_dir)
+                )
+            else: 
+                print('* * * Repo already exists.')
+                print('Run with "--force-reinit-git" to reinitialize repo')
             print_hr()
+
+
+        if deploy:
+            pass
+
 
 
